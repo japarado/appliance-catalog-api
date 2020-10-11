db98b351e4bd9deb441f86394430632f8e721dfe
Justin Parado <justin.parado.personal@gmail.com>
2020-10-11 22:12:20 +0800

    Removed password hashing functions from auth_controller

--- src/controllers/auth_controller.rs
+++ src/controllers/auth_controller.rs
@@ -1,5 +1,4 @@
-use actix_web::{delete, get, post, HttpRequest, HttpResponse, Responder};
-use std::env;
+use actix_web::{delete, post, HttpRequest, HttpResponse, Responder};
 
 #[post("/login")]
 pub async fn login(_req: HttpRequest) -> impl Responder {
@@ -14,19 +13,4 @@ pub async fn register(_req: HttpRequest) -> impl Responder {
 #[delete("/logout")]
 pub async fn logout(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().json("Logout")
-}
-
-use argon2::Config;
-fn create_hash(text: &String) -> String {
-    let text_to_hash = text.to_owned().into_bytes();
-    let salt = env::var("SALT")
-        .unwrap_or(String::from("Default salt value"))
-        .into_bytes();
-
-    let config = Config::default();
-    argon2::hash_encoded(&text_to_hash, &salt, &config).unwrap()
-}
-
-fn verify_hash(text: String, hash: String) -> bool {
-    return argon2::verify_encoded(&hash, &text.into_bytes()).unwrap();
 }
