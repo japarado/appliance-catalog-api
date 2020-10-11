7b9a450355d4189cca134e1c50c224e60a0b26fb
Justin Parado <justin.parado.personal@gmail.com>
2020-10-11 20:59:41 +0800

    Hash creator function

--- src/controllers/auth_controller.rs
+++ src/controllers/auth_controller.rs
@@ -1,4 +1,5 @@
 use actix_web::{delete, get, post, HttpRequest, HttpResponse, Responder};
+use std::env;
 
 #[post("/login")]
 pub async fn login(_req: HttpRequest) -> impl Responder {
@@ -13,4 +14,15 @@ pub async fn register(_req: HttpRequest) -> impl Responder {
 #[delete("/logout")]
 pub async fn logout(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().json("Logout")
+}
+
+use argon2::Config;
+fn create_hash(text: &String) -> String {
+    let text_to_hash = text.to_owned().into_bytes();
+    let salt = env::var("SALT") 
+        .unwrap_or(String::from("Default salt value"))
+        .into_bytes();
+
+    let config = Config::default();
+    argon2::hash_encoded(&text_to_hash, &salt, &config).unwrap()
 }
