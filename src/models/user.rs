3309abd3dfd35a0bfeaf37574cb834d9e0524538
Justin Parado <justin.parado.personal@gmail.com>
2020-10-12 12:46:13 +0800

    Made user struct fields public

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,24 +1,25 @@
 use crate::schema::*;
+use actix_web::web;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Identifiable, Queryable, Serialize, Deserialize)]
 pub struct User {
-    id: i32,
-    email: String,
-    password: String,
-    display_name: Option<String>,
-    profile_picture: Option<String>,
-    bio: Option<String>,
+    pub id: i32,
+    pub email: String,
+    pub password: String,
+    pub display_name: Option<String>,
+    pub profile_picture: Option<String>,
+    pub bio: Option<String>,
 }
 
 #[derive(Debug, Insertable, AsChangeset, Serialize, Deserialize)]
 #[table_name = "users"]
 pub struct NewUser {
-    email: String,
-    password: String,
-    display_name: Option<String>,
-    profile_picture: Option<String>,
-    bio: Option<String>,
+    pub email: String,
+    pub password: String,
+    pub display_name: Option<String>,
+    pub profile_picture: Option<String>,
+    pub bio: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
