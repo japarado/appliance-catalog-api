fb04a8aac15c0c11c80f85ed639e260ee13643b6
Justin Parado <justin.parado.personal@gmail.com>
2020-10-12 12:46:40 +0800

    Auth controller - registration route

--- src/controllers/auth_controller.rs
+++ src/controllers/auth_controller.rs
@@ -1,13 +1,31 @@
-use actix_web::{delete, post, HttpRequest, HttpResponse, Responder};
+use crate::controllers::ok_closure;
+use crate::database::Pool;
+use crate::errors::ServiceError;
+use crate::models::user::{NewUser, SlimUser, User};
+use crate::models::Single;
+use crate::services::auth_service;
+use crate::utils;
+use crate::AppData;
+use actix_web::error::BlockingError;
+use actix_web::{delete, post, web, HttpRequest, HttpResponse, Responder};
 
 #[post("/login")]
-pub async fn login(_req: HttpRequest) -> impl Responder {
+pub async fn login(data: web::Data<AppData>, payload: web::Json<User>) -> impl Responder {
     HttpResponse::Ok().json("Login")
 }
 
 #[post("/register")]
-pub async fn register(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok().json("Register")
+pub async fn register(data: web::Data<AppData>, payload: web::Json<NewUser>) -> impl Responder {
+    web::block(move || -> Result<User, ServiceError> {
+        let conn = data.conn_pool.get().unwrap();
+        Ok(auth_service::register(&conn, payload.into())?)
+    })
+    .await
+    .map(|data| ok_closure(data))
+    .map_err(|err| match err {
+        BlockingError::Error(service_error) => service_error,
+        BlockingError::Canceled => ServiceError::InternalServerError,
+    })
 }
 
 #[delete("/logout")]
