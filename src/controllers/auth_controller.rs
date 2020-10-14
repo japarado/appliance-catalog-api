e0f3ec706a77a41d69ce7f06e369601fc2dde8c8
Justin Parado <justin.parado.personal@gmail.com>
2020-10-14 18:54:56 +0800

    Login controller route

--- src/controllers/auth_controller.rs
+++ src/controllers/auth_controller.rs
@@ -1,32 +1,31 @@
 use crate::controllers::ok_closure;
 use crate::database::Pool;
 use crate::errors::ServiceError;
-use crate::models::user::{NewUser, SlimUser, User, LoginUser};
+use crate::models::user::{LoginUser, NewUser, SlimUser, User};
 use crate::models::Single;
 use crate::services::auth_service;
 use crate::utils;
 use crate::AppData;
+use actix_identity::Identity;
 use actix_web::error::BlockingError;
 use actix_web::{delete, post, web, HttpRequest, HttpResponse, Responder};
-use actix_identity::Identity;
 
 #[post("/login")]
-pub async fn login(data: web::Data<AppData>, payload: web::Json<LoginUser>, identity: Identity) -> impl Responder {
+pub async fn login(
+    data: web::Data<AppData>,
+    payload: web::Json<LoginUser>,
+    identity: Identity,
+) -> impl Responder {
     web::block(move || -> Result<User, ServiceError> {
         let conn = data.conn_pool.get().unwrap();
         Ok(auth_service::login(&conn, payload.into())?)
     })
     .await
-        .map(|data| {
-            let token = auth_service::generate_jwt(data.clone().into()).unwrap();
-            println!("JWT Generated: {}", token);
-            identity.remember(token.clone());
-
-            let token_decoded = utils::decode_token(&token).unwrap();
-
-            println!("Token Decoded: {:?}", token_decoded);
-            ok_closure(data)
-        })
+    .map(|login_user| {
+        let token = auth_service::generate_jwt(login_user.clone().into()).unwrap();
+        identity.remember(token);
+        ok_closure(login_user)
+    })
     .map_err(|err| match err {
         BlockingError::Error(service_error) => service_error,
         BlockingError::Canceled => ServiceError::InternalServerError,
